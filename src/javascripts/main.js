01fd46eaa8b4eaf10b3a7d92dafd3b86e13984ab
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-06-12 16:34:25 -0500

    Added New Messages

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,14 +2,18 @@ import '../styles/main.scss';
 import '../styles/messagesDiv.scss';
 import 'bootstrap';
 import messages from './components/messageUsers';
-import navbar from './navbar';
+import navbar from './components/navbar';
 import userList from './components/userList';
+import addMessage from './components/addMessage';
 
 const init = () => {
   userList.createUserButtons();
   userList.addEventListeners();
   messages.displayMessages();
   navbar.makeNavbar();
+  addMessage.textArea();
+  addMessage.submit();
+  addMessage.createNewMessage();
 };
 
 init();
