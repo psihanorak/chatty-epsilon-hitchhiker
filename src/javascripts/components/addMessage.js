9993a9963843682bdd4a4869fa9343796095dc6d
Pete Stewart <petestew@gmail.com>
2020-06-13 11:27:47 -0500

    wip

--- src/javascripts/components/addMessage.js
+++ src/javascripts/components/addMessage.js
@@ -2,23 +2,29 @@ import utils from '../helpers/utils';
 import messageData from '../helpers/data/messageData';
 import messageUsers from './messageUsers';
 // import userList from './userList';
+import userData from '../helpers/data/userData';
 
 const textArea = () => {
   let domString = '';
 
   domString += `<form id="form">
+                <div>
                 <textarea id="text" name="textarea_field" placeholder="Type here..." rows="10" cols="80"></textarea>
+                </div>
+                <div>
                 <button id="submit" type="button" class="btn btn-light">Submit</button>
+                </div>
                 </form>`;
   utils.printToDom('#text-area', domString);
 };
 
 const createNewMessage = () => {
+  const currentUser = userData.getCurrentUser();
   const message = document.getElementById('text').value;
   // const user = userList.addEventListeners();
   const newMessage = {
     timestamp: Date.now(),
-    sender: '',
+    sender: currentUser,
     text: message,
   };
   messageData.getMessages().push(newMessage);
